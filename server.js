5bd3ba8d464f5d972f91e536999e31b05727169d
jrdrenth <john.drenth@gmail.com>
2021-04-25 16:24:09 -0700

    Added initial server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const hbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// // Set up Handlebars.js engine with custom helpers
+// const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.set('view engine', 'hbs');
+app.engine('hbs',
+  hbs({
+      extname: 'hbs',
+      defaultLayout: 'main'
+  })
+);
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, (err) => {
+      if (err) {
+          console.log(`ERR: ${err.message}, ${err.stack}`);
+      }
+      console.log(`Server started on port ${PORT}`);
+  });
+});
